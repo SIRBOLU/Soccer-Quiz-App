3cf75d4e39a3dc694917eff5cb36bdf70eda93cc
Sirbolu <peterbolu@yahoo.com>
2026-01-03 21:57:20 +0100

    log in and sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 // import "./index.css";
 
 function App() {
+  const [increase, setIncrease] = useState(0);
+  const [displayLogIn, setDisplayLogIn] = useState(false);
+
+  const handleIncrease = () => {
+    setIncrease(increase + 1);
+  };
+  const handleDecrease = () => {
+    setIncrease(increase - 1);
+  };
   return (
     <>
       {/* <header className="flex justify-between bg-green-900 w-[100%] h-14 px-4 py-4 ">
@@ -87,14 +97,18 @@ function App() {
             Welcome to Soccer Quiz Game
           </h1>
           <h2 className="text-white italic text-center px-4 pt-2 lg:text-3xl md:text-2xl text-md">
-            Test Your Knowledge About This Game of Soccer
+            Test Your Knowledge About The Game of Soccer
           </h2>
+
+          {/* SIGN IN */}
           <h3 className="text-white pt-6 pb-3">
             Don't have an account?
             <button className="bg-white text-green-900 font-semibold px-2 py-1 ml-2 rounded-lg hover:bg-green-900 hover:text-white hover:border-2 border-white">
               Sign Up
             </button>
           </h3>
+
+          {/* LOG IN */}
           <h3 className="text-white">
             Already have an account?
             <button className="bg-white text-green-900 font-semibold px-2 py-1 ml-2 rounded-lg hover:bg-green-900 hover:text-white hover:border-2 border-white">
@@ -103,6 +117,93 @@ function App() {
           </h3>
         </div>
       </section>
+      {/* SIGN UP FORM */}
+      <div className="bg-green-400 w-[400px] m-auto mt-4">
+        <h1 className="text-center text-3xl pt-4 pb-2 font-bold">Sign Up</h1>
+        <div className="w-16 h-1 bg-white m-auto mb-4 rounded-lg"></div>
+        <form
+          className="flex flex-col items-left w-[200px] pl-10 pb-6"
+          action=""
+        >
+          <label className="" htmlFor="">
+            First Name
+          </label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="text"
+            placeholder="First Name"
+          />
+          <label htmlFor="">Last Name</label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="text"
+            placeholder="Last Name"
+          />
+          <label htmlFor="">Username</label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="text"
+            placeholder="Username"
+          />
+          <label htmlFor="">Email</label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="email"
+            placeholder="Email"
+          />
+          <label htmlFor="">Preferred Password</label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="password"
+            placeholder="Preferred Password"
+          />
+        </form>
+        <div className="w-[400px] flex justify-center">
+          <button
+            className="bg-green-900 text-white p-2 rounded-lg hover:text-green-800 hover:bg-white mb-6 font-semibold pl-4 pr-4"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+
+      {/* LOG IN FORM */}
+
+      <div className="bg-green-400 w-[400px] m-auto mt-4">
+        <h1 className="text-center text-3xl pt-4 pb-2 font-bold">Log In</h1>
+        <div className="w-16 h-1 bg-white m-auto mb-4 rounded-lg"></div>
+        <form
+          className="flex flex-col items-left w-[200px] pl-10 pb-6"
+          action=""
+        >
+          <label htmlFor="">Username</label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="text"
+            placeholder="Username"
+          />
+          <label htmlFor="">Password</label>
+          <input
+            className="w-[250px] rounded-md p-1 pl-4 mt-2 mb-4"
+            type="password"
+            placeholder="Password"
+          />
+        </form>
+        <div className="w-[400px] flex justify-center">
+          <button
+            className="bg-green-900 text-white p-2 pl-4 pr-4 rounded-lg hover:text-green-800 hover:bg-white mb-6 font-semibold"
+            type="submit"
+          >
+            Log In
+          </button>
+        </div>
+      </div>
+
+      {/* UseState */}
+      <button onClick={handleIncrease}>Increase</button>
+      <p>Number:{increase} </p>
+      <button onClick={handleDecrease}>Decrease</button>
 
       <footer className="text-center bg-green-200 p-3 h-full">
         techlyCoded &copy; 2026
